b70a483cd964fe65a9fe578a75ca0928de6a81c7
nharikrishna <nharikrishna275@gmail.com>
2025-04-16 18:37:09 -0400

    minor change

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import session from "express-session";
 import mongoose from "mongoose";
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+console.log("Connection String" + CONNECTION_STRING)
 mongoose.connect(CONNECTION_STRING);
 
 const app = express()
